f3441e71b6038aeab84a20f6d2b9139173cc38a9
vadim <romanskyvadim@gmail.com>
2019-04-18 17:42:06 +0300

    particle output

--- plot_particle_trajectory.m
+++ plot_particle_trajectory.m
@@ -9,6 +9,20 @@ Bx = hdf5read(full_name,'bx');
 By = hdf5read(full_name,'by');
 Bz = hdf5read(full_name,'bz');
 fileinfo = hdf5info(full_part_name);
+last_number = 10;
+a = last_number;
+
+if(a < 10)
+        full_name = strcat(directory_name, file_name, '.00', num2str(a));
+        full_part_name = strcat(directory_name, part_name, '.00', num2str(a));
+    else if (a < 100)
+            full_name = strcat(directory_name, file_name, '.0', num2str(a));
+            full_part_name = strcat(directory_name, part_name, '.0', num2str(a));  
+        else 
+            full_name = strcat(directory_name, file_name, '.', num2str(a));
+            full_part_name = strcat(directory_name, part_name, '.', num2str(a));
+        end;
+    end;
 
 gammae = hdf5read(full_part_name, 'gammae');
 xe = hdf5read(full_part_name, 'xe');
@@ -28,18 +42,27 @@ B0 = 0.03030750;
 Nx = size(Bx, 1);
 Ny = size(By, 2);
 
-last_number = 26;
 frameTime = 1.0/last_number;
 
-part_number = 31846;
+maxGamma = 1.0;
+max_number = 1;
+for i = 1: size(gammae,1),
+    if (gammae(i) > maxGamma)
+        max_number = i;
+        maxGamma = gammae(i);
+    end;
+end;
 
-Bnorm(1:Nx/2, 1:Ny) = 0;
+part_number = max_number;
+
+Bnorm(1:Ny, 1:Nx) = 0;
 
 
 
-for i=1:Nx/2,
+
+for i=1:Nx,
     for j = 1:Ny,
-        Bnorm(i,j) = sqrt(Bx(i,j)*Bx(i,j) + By(i,j)*By(i,j) + Bz(i,j)*Bz(i,j));
+        Bnorm(j,i) = sqrt(Bx(i,j)*Bx(i,j) + By(i,j)*By(i,j) + Bz(i,j)*Bz(i,j));
        % Bperp(i,j) = sqrt(By(i,j)*By(i,j) + Bz(i,j)*Bz(i,j));
     end;
 end;
@@ -63,11 +86,11 @@ n = 10^-4;
 omega = sqrt(4*pi*n*q*q/me);
 
 rho = c0/(omega*Nskinlength);
-rho = 0.2;
+rho = 0.1;
 c1=0.45;
 
 tau = c1*rho/c0;
-samplingFactor = 5;
+samplingFactor = 1;
 fieldFactor = me*rho/(q*tau*tau);
 rho = rho*samplingFactor;
 
@@ -96,14 +119,14 @@ grid on;
 hold on;
 %axis([Xgrid(1) Xgrid(Nx-1) minEx maxEx]);
 %fig = plot (Xgrid(1:Nx-1),Ex(1:Nx-1), 'red');
-fig = imagesc(1:Ny,1:Nx/2,Bnorm);
-fig_part = plot(ye(part_number), xe(part_number), 'ro', 'MarkerSize', 30);
-pos = get(gcf, 'Position');
-width = pos(3);
-height = pos(4);
-mov(1:height, 1:width, 1:1, 1:last_number)=0;
-f = getframe(gcf);
-[mov(:,:,1,1), map]=rgb2ind(f.cdata, colorcube(256));
+fig = imagesc((1:Nx)*samplingFactor, (1:Ny)*samplingFactor,Bnorm);
+fig_part = plot(xe(part_number), ye(part_number), 'ro', 'MarkerSize', 30);
+%pos = get(gcf, 'Position');
+%width = pos(3);
+%height = pos(4);
+%mov(1:height, 1:width, 1:1, 1:last_number)=0;
+%f = getframe(gcf);
+%[mov(:,:,1,1), map]=rgb2ind(f.cdata, colorcube(256));
 for a = 1:last_number,
     if(a < 10)
         full_name = strcat(directory_name, file_name, '.00', num2str(a));
@@ -116,9 +139,9 @@ for a = 1:last_number,
             full_part_name = strcat(directory_name, part_name, '.', num2str(a));
         end;
     end;
-    for i=1:Nx/2,
+    for i=1:Nx,
         for j = 1:Ny,
-            Bnorm(i,j) = sqrt(Bx(i,j)*Bx(i,j) + By(i,j)*By(i,j) + Bz(i,j)*Bz(i,j));
+            Bnorm(j,i) = sqrt(Bx(i,j)*Bx(i,j) + By(i,j)*By(i,j) + Bz(i,j)*Bz(i,j));
         % Bperp(i,j) = sqrt(By(i,j)*By(i,j) + Bz(i,j)*Bz(i,j));
         end;
     end;
@@ -133,11 +156,11 @@ for a = 1:last_number,
     
     set(fig, 'CData', Bnorm);
     
-    set(fig_part, 'Ydata', xe(part_number));
-    set(fig_part, 'Xdata', ye(part_number));
-    f = getframe(gcf);
-    mov(:,:,1,a+1)=rgb2ind(f.cdata, map);
-    pause(frameTime);
+    set(fig_part, 'Xdata', xe(part_number));
+    set(fig_part, 'Ydata', ye(part_number));
+    %f = getframe(gcf);
+    %mov(:,:,1,a+1)=rgb2ind(f.cdata, map);
+    pause(frameTime*10);
 end;
-imwrite(mov, map, 'Ex.gif','DelayTime',frameTime,'LoopCount',1);
+%imwrite(mov, map, 'Ex.gif','DelayTime',frameTime,'LoopCount',1);
 
