8f3b99e7c43f3c4768eb9362e2aaec72907fdaf6
romanskyvadim <romanskyvadim@gmail.com>
2021-02-10 00:12:16 +0300

    bug in image

--- plot_image.m
+++ plot_image.m
@@ -2,16 +2,17 @@ clear;
 data = importdata('output/image.dat');
 Nr = size(data,1)/4;
 Nphi = size(data,2);
-image(1:Nr,1:Nphi) = 0;
+image(1:Nphi+1,1:Nr) = 0;
 for i = 1:Nr,
     for j = 1:Nphi,
-        image(i,j) = data(i,j);
+        image(j,i) = data(i,j);
     end;
+    image(Nphi+1,i) = data(i,1);
 end;
 
 
 figure(1)
-[r,t] = meshgrid(1:Nr,0:2*pi/Nphi:2*pi);
+[r,t] = meshgrid(1:Nr,pi/Nphi:2*pi/Nphi:2*pi+pi/Nphi);
 x = r.*cos(t);
 y = r.*sin(t);
 contourf(x,y,image);
@@ -21,5 +22,5 @@ plot(90*((0:0.33:1)'*cosd(0:10:360))', 90*((0:0.33:1)'*sind(0:10:360))','k')
 colorbar
 set(colorbar,'FontSize',16)
 axis equal
-set(gca, 'Box','off', 'XColor','none', 'YColor','none',  'Color','none')
+%set(gca, 'Box','off', 'XColor','none', 'YColor','none',  'Color','none')
 hold off
